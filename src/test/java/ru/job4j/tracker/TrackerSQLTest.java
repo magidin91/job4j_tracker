800ac72e7765d63708f22ffdf0424fbce2398c86
magidin91 <magidin@mail.ru>
2020-05-03 17:34:47 +0500

    4.5. Travis CI и PostgreSQL

--- src/test/java/ru/job4j/tracker/TrackerSQLTest.java
+++ src/test/java/ru/job4j/tracker/TrackerSQLTest.java
@@ -4,7 +4,7 @@ import org.junit.Test;
 
 import java.util.List;
 
-import static org.hamcrest.Matchers.is;
+import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
 
 public class TrackerSQLTest {
