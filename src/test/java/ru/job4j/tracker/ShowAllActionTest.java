3ed097113666baadd04b3365e226f079e70a7dad
magidin91 <magidin@mail.ru>
2020-05-19 21:04:41 +0500

    5. Restructuring. Added Javadoc.2

--- src/test/java/ru/job4j/tracker/ShowAllActionTest.java
+++ src/test/java/ru/job4j/tracker/ShowAllActionTest.java
@@ -1,5 +1,7 @@
 package ru.job4j.tracker;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import ru.job4j.tracker.actions.ShowAllAction;
 import ru.job4j.tracker.input.StubInput;
@@ -13,22 +15,34 @@ import java.util.StringJoiner;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
+/**
+ * Redirecting "standard" output stream to an array of bytes
+ */
 public class ShowAllActionTest {
+    private final PrintStream stdout = System.out;
+    private final ByteArrayOutputStream out = new ByteArrayOutputStream();
+
+    @Before
+    public void loadOutput() {
+        System.setOut(new PrintStream(this.out));
+    }
+
+    @After
+    public void backOutput() {
+        System.setOut(this.stdout);
+    }
 
     @Test
     public void whenCheckOutput() {
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
-        PrintStream std = System.out;
-        System.setOut(new PrintStream(out));
         Tracker tracker = new Tracker();
         Item item = new Item("fix bug");
         tracker.add(item);
         ShowAllAction act = new ShowAllAction();
         act.execute(new StubInput(new String[]{}), System.out::println, tracker);
         String expect = new StringJoiner(System.lineSeparator(), "", System.lineSeparator())
-                .add("id=" + item.getId() + ", name=" + item.getName())
+                .add("=== Done ====")
+                .add(String.format("Item item: name = %s, id = %s", item.getName(), item.getId()))
                 .toString();
         assertThat(new String(out.toByteArray()), is(expect));
-        System.setOut(std);
     }
 }
